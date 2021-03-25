40f285531b60a375316d57012ca38c06b07e24c3
sabu-t0060 <sabut@student.douglascollege.ca>
2021-03-25 15:33:05 -0700

    adding controller and extention

---
+++ RosePurchaseManagementCodeFirstFromDB/SeedDatabaseExtentionMethods.cs
@@ -0,0 +1,57 @@
+﻿using RosePurchaseManagementCodeFirstFromDB;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeedDatabaseExtensions
+{
+
+	public static class SeedDatabaseExtensionMethods
+	{
+		/// <summary>
+		/// zero out the db tables, then seed all tables with initial data
+		/// </summary>
+		public static void SeedDatabase(this RosePurchaseManagementEntities context)
+		{
+			// set up database log to write to output window in VS
+			context.Database.Log = (s => Debug.Write(s));
+
+			// reset the db
+			context.Database.Delete();
+			context.Database.Create();
+
+			context.SaveChanges();
+
+			// another way to reinitialize the database, resetting everything and zeroing out data
+
+			//Database.SetInitializer(new DropCreateDatabaseAlways<StudentRegistrationEntities>());
+			//context.Database.Initialize(true);        
+
+			context.Boxes.Load();
+			context.BoxInventories.Load();
+			context.BoxPurchases.Load();
+			context.Farms.Load();
+			context.Groups.Load();
+			context.Inventories.Load();
+			context.Invoices.Load();
+			context.Orders.Load();
+			context.Purchases.Load();
+			context.Roses.Load();
+			context.RoseSizes.Load();
+			context.Sizes.Load();
+			context.Warehouses.Load();
+
+
+
+
+
+
+
+			context.SaveChanges();
+		}
+	}
+}
