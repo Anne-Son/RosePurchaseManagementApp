37d52dc0337a4034d415c27480b254e033a804df
Anne Son <annesontech@gmail.com>
2021-03-30 17:48:08 -0700

    name changed

--- ProjectTeam05RosePurchaseManagement/PurchasingAgentForm.cs
+++ ProjectTeam05RosePurchaseManagement/PurchasingAgentForm.cs
@@ -27,12 +27,13 @@ namespace ProjectTeam05RosePurchaseManagement
 
             this.Load += (s, e) => PurchasingAgentForm_Load();
 
+
             //Set button Click events for operating on Purchase table. Make use of a generic method
             //(AddOrUpdateForm) and DeleteForm
 
             //set up the button handlers
 
-           // buttonPurchase.Click += (s, e) => PurchaseFromInventory();
+            // buttonPurchase.Click += (s, e) => PurchaseFromInventory();
         }
         /// <summary>
         /// When the form loads, seed the db table, set up datagridview controles, and display
@@ -40,8 +41,16 @@ namespace ProjectTeam05RosePurchaseManagement
         /// </summary>
         private void PurchasingAgentForm_Load()
         {
-          
+            
+           // context.Purchases.Include(d => d.BoxPurchases).Load();
+
             InitializeDataGridView();
+
+
+            dataGridViewPurchase.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewPurchase.ReadOnly = true;
+            dataGridViewPurchase.AllowUserToAddRows = false;
+            dataGridViewPurchase.DataSource = GetPurchaseBoxQuantities();
 
             //display the data in the purchase gridview
 
@@ -50,21 +59,43 @@ namespace ProjectTeam05RosePurchaseManagement
 
         private void InitializeDataGridView()
         {
-            //Composite are not editable. Done this on control
-            //Set up cell validation for each. Row validation for each, as a user
-            //might have left the row due to a focus change, and need to check to see
-            // if entity is complete and valid.
+            RosePurchaseManagementEntities context = new RosePurchaseManagementEntities();  
 
-            InitializeDataGridView<Order>(dataGridViewOrder);
-            InitializeDataGridView<Inventory>(dataGridViewInventoryPurchase, "RoseSize", "Farm");
-            InitializeDataGridView<Purchase>(dataGridViewPurchase,"Farm","Invoice","RoseSize","Warehouse");
+           InitializeDataGridView<Order>(dataGridViewOrder, "RoseSize");
+           InitializeDataGridView<Inventory>(dataGridViewInventoryPurchase, "RoseSize", "Farm");
+            // InitializeDataGridView<Purchase>(dataGridViewPurchase,"Farm","Invoice","RoseSize","Warehouse");
+           // DisplayPurchase();
+            
 
-            dataGridViewPurchase.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            dataGridViewPurchase.ReadOnly = true;
-            dataGridViewPurchase.AllowUserToAddRows = false;
+            
+            
 
         }
 
+       List<PurchaseBoxQuantity> GetPurchaseBoxQuantities()
+        {
+            List<PurchaseBoxQuantity> purchaseBoxQuantities = new List<PurchaseBoxQuantity>();
+            List<Purchase> purchases = (List<Purchase>)Controller<RosePurchaseManagementEntities, Purchase>.GetEntitiesWithIncluded("BoxPurchases");
+            foreach(Purchase purchase in purchases)
+            {
+                foreach(BoxPurchase boxPurchase in purchase.BoxPurchases)
+                {
+                    PurchaseBoxQuantity purchaseBoxQuantity = new PurchaseBoxQuantity()
+                    {
+                        PurchaseID = purchase.PurchaseID,
+                        FarmID = purchase.FarmID,
+                        RoseID = purchase.RoseSizeID,
+                        Price = purchase.Price_per_stem,
+                        InvoiceNumber = purchase.InvoiceID,
+                        WarehouseID = purchase.WarehouseID,
+                        BoxQuantity = boxPurchase.Quantity
+                    };
+                    purchaseBoxQuantities.Add(purchaseBoxQuantity);
+                }
+            }
+            return purchaseBoxQuantities;
+
+        }
         /// <summary>
         /// Common generic method to initialize datagridview controls,sets the datasource, autosizes the control to the columns.
         /// </summary>
@@ -80,7 +111,7 @@ namespace ProjectTeam05RosePurchaseManagement
 
             dataGridView.DataError += (s, e) => HandleDataError<T>(s as DataGridView, e);
             dataGridView.DataSource = Controller<RosePurchaseManagementEntities, T>.SetBindingList();
-           
+           // dataGridView.DataSource = Controller<RosePurchaseManagementEntities, T>.GetEntitiesWithIncluded(entities);
 
             //do not show certain columns
             foreach (string column in columnsToHide)
@@ -90,6 +121,34 @@ namespace ProjectTeam05RosePurchaseManagement
         {
             Debug.WriteLine("DataError " + typeof(T) + " " + gridView.Name + " row " + e.RowIndex + " col " + e.ColumnIndex + " Context: " + e.Context.ToString());
             e.Cancel = true;
+        }
+
+        private class PurchaseBoxQuantity
+        {
+            [DisplayName("PurchaseID")]
+            public int PurchaseID { get; set; }
+
+            [DisplayName("FarmID")]
+            public int FarmID { get; set; }
+
+            [DisplayName("RoseID")]
+            public int RoseID { get; set; }
+
+            [DisplayName("Price")]
+            public float Price { get; set; }
+
+            [DisplayName("InvoiceNumber")]
+            public int? InvoiceNumber { get; set; }
+
+            [DisplayName("WarehouseID")]
+            public int WarehouseID { get; set; }
+
+            [DisplayName("BoxQuantity")]
+            public int BoxQuantity { get; set; }
+
+            public Purchase Purchase { get; set; }
+
+            public BoxPurchase BoxPurchase { get; set; }
         }
     }
 }
