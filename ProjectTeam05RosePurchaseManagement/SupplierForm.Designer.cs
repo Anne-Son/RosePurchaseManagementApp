e03d7863c8a58be7b39f56be24eadaadfa32a86b
Avneet Kaur Dhillon <dhillona20@student.douglascollege.ca>
2021-04-06 09:34:35 -0800

    Supplier form revised

--- ProjectTeam05RosePurchaseManagement/SupplierForm.Designer.cs
+++ ProjectTeam05RosePurchaseManagement/SupplierForm.Designer.cs
@@ -30,38 +30,82 @@ namespace ProjectTeam05RosePurchaseManagement
         private void InitializeComponent()
         {
             this.tabFlowers = new System.Windows.Forms.TabPage();
+            this.dataGridViewFlowers = new System.Windows.Forms.DataGridView();
             this.tabControlManager = new System.Windows.Forms.TabControl();
+            this.buttonAddOrUpdate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.tabFlowers.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFlowers)).BeginInit();
             this.tabControlManager.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabFlowers
             // 
-            this.tabFlowers.Location = new System.Drawing.Point(4, 25);
+            this.tabFlowers.Controls.Add(this.buttonDelete);
+            this.tabFlowers.Controls.Add(this.buttonAddOrUpdate);
+            this.tabFlowers.Controls.Add(this.dataGridViewFlowers);
+            this.tabFlowers.Location = new System.Drawing.Point(4, 29);
+            this.tabFlowers.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tabFlowers.Name = "tabFlowers";
-            this.tabFlowers.Padding = new System.Windows.Forms.Padding(3);
-            this.tabFlowers.Size = new System.Drawing.Size(998, 492);
+            this.tabFlowers.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabFlowers.Size = new System.Drawing.Size(1124, 618);
             this.tabFlowers.TabIndex = 1;
             this.tabFlowers.Text = "Flowers";
             this.tabFlowers.UseVisualStyleBackColor = true;
             // 
+            // dataGridViewFlowers
+            // 
+            this.dataGridViewFlowers.AllowUserToDeleteRows = false;
+            this.dataGridViewFlowers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewFlowers.Location = new System.Drawing.Point(25, 17);
+            this.dataGridViewFlowers.Name = "dataGridViewFlowers";
+            this.dataGridViewFlowers.RowHeadersWidth = 62;
+            this.dataGridViewFlowers.RowTemplate.Height = 28;
+            this.dataGridViewFlowers.Size = new System.Drawing.Size(918, 290);
+            this.dataGridViewFlowers.TabIndex = 0;
+            // 
             // tabControlManager
             // 
             this.tabControlManager.Controls.Add(this.tabFlowers);
-            this.tabControlManager.Location = new System.Drawing.Point(12, 12);
+            this.tabControlManager.Location = new System.Drawing.Point(14, 15);
+            this.tabControlManager.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tabControlManager.Name = "tabControlManager";
             this.tabControlManager.SelectedIndex = 0;
-            this.tabControlManager.Size = new System.Drawing.Size(1006, 521);
+            this.tabControlManager.Size = new System.Drawing.Size(1132, 651);
             this.tabControlManager.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
             this.tabControlManager.TabIndex = 2;
             // 
+            // buttonAddOrUpdate
+            // 
+            this.buttonAddOrUpdate.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAddOrUpdate.Location = new System.Drawing.Point(195, 363);
+            this.buttonAddOrUpdate.Name = "buttonAddOrUpdate";
+            this.buttonAddOrUpdate.Size = new System.Drawing.Size(199, 35);
+            this.buttonAddOrUpdate.TabIndex = 1;
+            this.buttonAddOrUpdate.Text = "Add / Update";
+            this.buttonAddOrUpdate.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDelete.Location = new System.Drawing.Point(594, 363);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(146, 35);
+            this.buttonDelete.TabIndex = 2;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            // 
             // SupplierForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1033, 534);
+            this.ClientSize = new System.Drawing.Size(973, 520);
             this.Controls.Add(this.tabControlManager);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "SupplierForm";
             this.Text = "SupplierForm";
+            this.tabFlowers.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFlowers)).EndInit();
             this.tabControlManager.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -71,5 +115,8 @@ namespace ProjectTeam05RosePurchaseManagement
 
         private System.Windows.Forms.TabPage tabFlowers;
         private System.Windows.Forms.TabControl tabControlManager;
+        private System.Windows.Forms.DataGridView dataGridViewFlowers;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonAddOrUpdate;
     }
 }
--- ProjectTeam05RosePurchaseManagement/SupplierForm.cs
+++ ProjectTeam05RosePurchaseManagement/SupplierForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EFControllerUtilities;
+using RosePurchaseManagementCodeFirstFromDB;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,84 @@ namespace ProjectTeam05RosePurchaseManagement
         public SupplierForm()
         {
             InitializeComponent();
+
+            //set up database and controls when form loads
+
+            this.Load += (s, e) => SupplierInventory_Load();
+        }
+
+        private void SupplierInventory_Load()
+        {
+            dataGridViewFlowers.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewFlowers.ReadOnly = true;
+            dataGridViewFlowers.AllowUserToAddRows = false;
+            dataGridViewFlowers.DataSource = GetSupplierInventory();
+            dataGridViewFlowers.Columns["Inventory"].Visible = false;
+            dataGridViewFlowers.Columns["BoxInventory"].Visible = false;
+        }
+
+
+        List<SupplierInventory> GetSupplierInventory()
+        {
+            List<SupplierInventory> supplierInventories = new List<SupplierInventory>();
+            List<Inventory> inventory = (List<Inventory>)Controller<RosePurchaseManagementEntities, Inventory>.GetEntitiesWithIncluded("BoxInventories", "Farm");
+            List<Box> box = (List<Box>)Controller<RosePurchaseManagementEntities, Box>.GetEntitiesWithIncluded("BoxInventories");
+
+            foreach (Inventory inventories in inventory)
+            {
+                foreach (BoxInventory boxes in inventories.BoxInventories)
+                {
+                    SupplierInventory supplierInventory = new SupplierInventory()
+                    {
+                        InventoryID = inventories.InventoryID,
+                        FarmID = inventories.FarmID, 
+                        FarmName = inventories.Farm.FarmName,
+                        RoseSizeID = inventories.RoseSizeID,
+                        Price = inventories.Price_per_stem,
+                        BoxID = boxes.BoxID,
+                        Quantity = boxes.Quantity,
+                        
+                    };
+                    using (RosePurchaseManagementEntities context = new RosePurchaseManagementEntities())
+                    {
+                        var roseName = context.RoseSizes.Include("Rose").Where(x => x.RoseSizeID == inventories.RoseSizeID).Select(r => r.Rose.RoseName).FirstOrDefault();
+                        supplierInventory.RoseName = roseName;
+                    }
+                        supplierInventories.Add(supplierInventory);
+                }
+            }
+            return supplierInventories;
+        }
+        private class SupplierInventory
+        {
+            [DisplayName("Inventory ID")]
+            public int InventoryID { get; set; }
+
+            [DisplayName("Farm ID")]
+            public int FarmID { get; set; }
+
+            [DisplayName("Farm Name")]
+            public string FarmName { get; set; }
+
+            [DisplayName("Rose Size ID")]
+            public int RoseSizeID { get; set; }
+
+            [DisplayName("Rose Name")]
+            public string RoseName { get; set; }
+
+            [DisplayName("Price per stem")]
+            public float Price { get; set; }
+
+            [DisplayName("Box ID")]
+            public int BoxID { get; set; }
+
+            [DisplayName("Quantity")]
+            public int Quantity { get; set; }
+
+            public Inventory Inventory { get; set; }
+
+            public BoxInventory BoxInventory { get; set; }
+
         }
     }
 }
