eafe3be28f6ef4c5c384d7d0d6a7d6563ee58a10
sabu-t0060 <sabut@student.douglascollege.ca>
2021-04-09 00:25:55 -0700

    last update Manager

--- ProjectTeam05RosePurchaseManagement/ManagerForm.Designer.cs
+++ ProjectTeam05RosePurchaseManagement/ManagerForm.Designer.cs
@@ -55,6 +55,9 @@ namespace ProjectTeam05RosePurchaseManagement
             this.buttonBackUp = new System.Windows.Forms.Button();
             this.listBoxWarehouse = new System.Windows.Forms.ListBox();
             this.listBoxRosesIn = new System.Windows.Forms.ListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.tabControlManager.SuspendLayout();
             this.tabPageOrder.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrder)).BeginInit();
@@ -166,6 +169,9 @@ namespace ProjectTeam05RosePurchaseManagement
             // 
             // tabPageReport
             // 
+            this.tabPageReport.Controls.Add(this.label9);
+            this.tabPageReport.Controls.Add(this.label8);
+            this.tabPageReport.Controls.Add(this.label7);
             this.tabPageReport.Controls.Add(this.listBoxRosesIn);
             this.tabPageReport.Controls.Add(this.listBoxWarehouse);
             this.tabPageReport.Controls.Add(this.labelAveragePrice);
@@ -322,6 +328,36 @@ namespace ProjectTeam05RosePurchaseManagement
             this.listBoxRosesIn.Size = new System.Drawing.Size(133, 196);
             this.listBoxRosesIn.TabIndex = 12;
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(41, 382);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(92, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "WareHouse";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(375, 382);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 17);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Roses";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(41, 81);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(76, 17);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Purchase";
+            // 
             // ManagerForm
             // 
             this.ClientSize = new System.Drawing.Size(1145, 699);
@@ -370,5 +406,8 @@ namespace ProjectTeam05RosePurchaseManagement
         private System.Windows.Forms.Button buttonBackUp;
         private System.Windows.Forms.ListBox listBoxRosesIn;
         private System.Windows.Forms.ListBox listBoxWarehouse;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
     }
 }
